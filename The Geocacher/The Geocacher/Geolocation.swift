bf255387bc3fbf0a86f354cfc3b33a5424e80a58
Trevor Harmon <tharmon@qualtrics.com>
2016-04-08 15:30:30 -0500

    Fixed Geolocation object so that it returns a subtitle (the category)

--- The Geocacher/The Geocacher/Geolocation.swift
+++ The Geocacher/The Geocacher/Geolocation.swift
@@ -13,44 +13,12 @@ import MapKit
 class Geolocation : NSObject, MKAnnotation {
     var id: Int
     var title: String?
+    var category: String?
+    
     var latitude: Double
     var longitude: Double
-    var category: String
     var coordinate: CLLocationCoordinate2D
-//    var viewLatitude: Double?
-//    var viewLongitude: Double?
-//    var viewTilt: Double?
-//    var viewRoll: Double?
-//    var viewAltitude: Double?
-//    var viewHeading: Double?
-    
-//    init(id: Int, name: String, latitude: Double, longitude: Double, category: String, viewLatitude: Double?, viewLongitude:Double?, viewTilt: Double?, viewRoll: Double?, viewAltitude: Double?, viewHeading: Double?) {
-//        self.id = id
-//        self.name = name
-//        self.latitude = latitude
-//        self.longitude = longitude
-//        self.category = category
-//        
-//        
-////        if let vLatitude = viewLatitude {
-////            self.viewLatitude = vLatitude
-////        }
-////        if let vLongitude = viewLongitude {
-////            self.viewLongitude = vLongitude
-////        }
-////        if let vTilt = viewTilt {
-////            self.viewTilt = vTilt
-////        }
-////        if let vRoll = viewRoll {
-////            self.viewRoll = vRoll
-////        }
-////        if let vAltitude = viewAltitude {
-////            self.viewAltitude = vAltitude
-////        }
-////        if let vHeading = viewHeading {
-////            self.viewHeading = vHeading
-////        }
-//    }
+
     init(id: Int, title: String, category: String, latitude: Double, longitude: Double) {
         self.id = id
         self.title = title
@@ -61,5 +29,10 @@ class Geolocation : NSObject, MKAnnotation {
         
         super.init()
     }
+    
+    var subtitle: String? {
+        return category
+    }
+
 
 }
